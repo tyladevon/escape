88c8dffe330975b44c7bcc309dfb698859a0d925
Alison Vermeil <alison.minton@gmail.com>
2020-02-20 16:07:50 -0700

    Fix error in factory_bot requirement

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rails'
-require 'support/factory_bot'
+require 'factory_bot'
 require 'simplecov'
 SimpleCov.start
 # Requires supporting ruby files with custom matchers and macros, etc, in
