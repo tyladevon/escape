bbe2e3ed668ca95efd192a7735900b6ba20397da
Alison Vermeil <alison.minton@gmail.com>
2020-02-20 18:05:21 -0700

    Add users controller and preliminary code for create
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,36 @@
-class User
+class UsersController < ApplicationController
+  def create
+    google_auth_hash = request.env["omniauth.auth"]
+    google_token = google_auth_hash["credentials"]
+    uid = google_auth_hash["uid"]
+    user_info = google_auth_hash["info"]
+    #name, email, first_name, last_name, image, google_token
+
+    user = User.create(
+                      first_name: user_info["first_name"],
+                      last_name: user_info["last_name"],
+                      email: user_info["email"],
+                      image: user_info["image"],
+                      google_token: google_token)
+
+    redirect_to '/profile'
+  end
+
+  private
+
+  # def google_auth_hash
+  #   request.env["omniauth.auth"]
+  # end
+  #
+  # def google_token
+  #   google_auth_hash["credentials"]
+  # end
+  #
+  # def uid
+  #   google_auth_hash["uid"]
+  # end
+  #
+  # def user_info
+  #   google_auth_hash["info"]
+  # end
+end
