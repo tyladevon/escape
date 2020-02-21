6b07e3cd921a2833921d56cd9aeca34a74b6cdc0
Alison Vermeil <alison.minton@gmail.com>
2020-02-20 22:44:08 -0700

    Add assertion for nav bar text

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -8,20 +8,26 @@ RSpec.describe 'As a user' do
 
     it 'I can logout' do
       visit '/'
-      
+
       within "nav" do
         expect(page).to_not have_link('Log Out')
       end
 
-
       click_button 'Sign-In with Google'
 
+      user = User.last
+
       within "nav" do
+        expect(page).to have_content("Hi, #{user.first_name}")
         click_link('Log Out')
       end
 
       expect(page).to have_content('You have been logged out.')
       expect(current_path).to eq('/')
+
+      within "nav" do
+        expect(page).to_not have_content("Hi, #{user.first_name}")
+      end
     end
   end
 end
