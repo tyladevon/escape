9c08774d1e202e1ab03661b9f63fd3bc5e36f20a
Alison Vermeil <alison.minton@gmail.com>
2020-02-22 14:08:19 -0700

    Continue to imrove destination search spec
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/features/users/search/destination_search_spec.rb
+++ spec/features/users/search/destination_search_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'As a logged in user' do
   describe 'I can search for a destination' do
     describe 'and enter my first activity' do
-      it 'from the search page' do
+      it 'as climbing' do
         user = create(:user)
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -11,12 +11,27 @@ RSpec.describe 'As a logged in user' do
         visit '/search'
 
         expect(page).to have_field("destination")
-        expect(page).to have_button("Continue")
+        fill_in "destination", with: "Los Angeles"
+
         expect(page).to have_content("Choose your first activity:")
         # expect the page to have the check boxes
-        expect(page).to have_unchecked_field("climbing")
-        expect(page).to have_unchecked_field("hiking")
-      end 
+        within "#activity-climbing" do
+          expect(page).to have_unchecked_field("selected_activity_")
+          check "selected_activity_"
+        end
+
+        within "#activity-hiking" do
+          expect(page).to have_unchecked_field("selected_activity_")
+        end
+
+        expect(page).to have_button("Continue")
+
+        click_button "Continue"
+
+        expect(current_path).to eq('/search/climb')
+
+        expect(page).to have_content('Coming Soon!')
+      end
     end
   end
 end
