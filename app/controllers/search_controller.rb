2f3b9c1d5e02f1bc48d75f86d9050aa9e1e835ad
Alison Vermeil <alison.minton@gmail.com>
2020-02-22 14:13:37 -0700

    Make connection to destination endpoint, to be refactored
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,8 +9,8 @@ class SearchController < ApplicationController
     response = Faraday.get("https://escape-app-api.herokuapp.com/api/v1/destination/#{destination}")
 
     parsed_json = JSON.parse(response.body)
-    # location = GooglePlaceService.new(params['destination'])
 
+    # add location poro 
     if params[:selected_activity].first == "climbing"
 
       redirect_to '/search/climb'
