a1b313183d9f2e893a46cef71ebb27b0a9d7656d
Madelyn Romero <m.romero013@gmail.com>
2020-02-23 14:33:57 -0700

    Add get and response methods for climb results

--- app/services/escape_service.rb
+++ app/services/escape_service.rb
@@ -9,6 +9,15 @@ class EscapeService
     end
   end
 
+  def get_climb_results(params, location)
+    response = climbs_response(params, location)
+    if response.status == 404
+      return response.status
+    else
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+
   private
 
   def connection
@@ -20,6 +29,12 @@ class EscapeService
   def location_response(destination)
     connection.get do |req|
       req.url ("destination/#{destination}")
+    end
+  end
+
+  def climbs_response(climb_preferences, location)
+    connection.get do |req|
+      req.url("climb_results")
     end
   end
 end
