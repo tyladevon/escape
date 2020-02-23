c09e217dd39971bb77160d6ad772ff4634e84cab
Tyla Devon <tyladevon@gmail.com>
2020-02-22 17:03:10 -0700

    Co-authored-by: m.romero013@gmail.com
    Add create method to fill in with Climb API call.

--- app/controllers/search/climbs_controller.rb
+++ app/controllers/search/climbs_controller.rb
@@ -1,5 +1,12 @@
 class Search::ClimbsController < ApplicationController
+
   def new
     @destination = session[:destination]
   end
+
+  def create
+    binding.pry
+    # response = Faraday.get("https://escape-app-api.herokuapp.com/api/v1/climb_results") do |req|
+      # req[:lat] = params[@]
+  end 
 end
