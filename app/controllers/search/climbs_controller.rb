8010113d4359adbad5aadd7ebf501c088364d755
Madelyn Romero <m.romero013@gmail.com>
2020-02-23 13:47:44 -0700

    Create initial call to escape microservice for climbs

--- app/controllers/search/climbs_controller.rb
+++ app/controllers/search/climbs_controller.rb
@@ -5,8 +5,14 @@ class Search::ClimbsController < ApplicationController
   end
 
   def create
+    response = Faraday.get("https://escape-app-api.herokuapp.com/api/v1/climb_results") do |req|
+      req[:lat] = params[:lat]
+      req[:lon] = params[:lon]
+      req[:max_dist] = params[:max_dist]
+      req[:min_diff] = params[:min_diff]
+      req[:max_diff] = params[:max_diff]
+    end
     binding.pry
-    # response = Faraday.get("https://escape-app-api.herokuapp.com/api/v1/climb_results") do |req|
-      # req[:lat] = params[@]
-  end 
+
+  end
 end
