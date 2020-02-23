715a133b0ef5a1029daf7661648c041add8cc97a
Madelyn Romero <m.romero013@gmail.com>
2020-02-23 14:54:29 -0700

    Comment out create method, instead make API call through index action

--- app/controllers/search/climbs_controller.rb
+++ app/controllers/search/climbs_controller.rb
@@ -4,15 +4,18 @@ class Search::ClimbsController < ApplicationController
     @destination = Destination.new(session[:destination])
   end
 
-  def create
-    location = session[:destination]
-
-    EscapeService.new.get_climb_results(params, location)
-    redirect_to "/search/climbs"
-  end
+  # def create
+  #   location = session[:destination]
+  #
+  #   EscapeService.new.get_climb_results(params, location)
+  #   redirect_to "/search/climbs"
+  # end
 
   def index
-    
+    location = session[:destination]
+    render locals: {
+      climb_results: EscapeService.new.get_climb_results(params, location)
+    }
   end
 
 end
