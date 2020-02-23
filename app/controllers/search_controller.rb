8407485f4927f980ce49288b18f123539785083c
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 10:36:50 -0700

    Refactor controller and use hew helper method for activities

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,30 +1,46 @@
 class SearchController < ApplicationController
   def new
-    @activities = ["climbing", "hiking"]
+    available_activities
   end
 
   def create
     destination = params[:destination].delete(' ')
-
     destination_info = EscapeService.new.get_destination_info(destination)
     if destination_info == 404 || destination_info.nil?
-      flash[:notice] = "The destination you entered cannot be found. Please try again."
-      @activities = ["climbing", "hiking"]
-      render :new
+      destination_error
     elsif params[:selected_activity].nil?
-      flash[:notice] = "Please select one activity."
-      @activities = ["climbing", "hiking"]
-      render :new
+      activity_selector_error
     else
-      name = destination_info[:name]
-      address = destination_info[:formatted_address]
-      lat = destination_info[:geometry][:location][:lat]
-      lng = destination_info[:geometry][:location][:lng]
-      session[:destination] = { name: name, address: address, lat: lat, lng: lng}
-
-      if params[:selected_activity].first == "climbing"
-        redirect_to '/search/climbs/new'
-      end
+      save_destination_to_session(destination_info)
+      redirect_to_activity_search
     end
+  end
+
+  private
+
+  def save_destination_to_session(destination_info)
+    name = destination_info[:name]
+    address = destination_info[:formatted_address]
+    lat = destination_info[:geometry][:location][:lat]
+    lng = destination_info[:geometry][:location][:lng]
+    session[:destination] = { name: name, address: address, lat: lat, lng: lng}
+  end
+
+  def redirect_to_activity_search
+    if params[:selected_activity].first == "climbing"
+      redirect_to '/search/climbs/new'
+    end
+  end
+
+  def destination_error
+    flash[:notice] = "The destination you entered cannot be found. Please try again."
+    available_activities
+    render :new
+  end
+
+  def activity_selector_error
+    flash[:notice] = "Please select one activity."
+    available_activities
+    render :new
   end
 end
