49b2b9e6b3fd97ae9a24df5078f41069fdcf4914
Tyla Devon <tyladevon@gmail.com>
2020-02-22 17:02:35 -0700

    Co-authored-by: m.romero013@gmail.com
    Create form for Climb Preferences

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def climb_diff
+    [
+      [5.6, 5.6],
+      [5.7, 5.7],
+      [5.8, 5.8],
+      [5.9, 5.9],
+      [5.10, 5.10],
+      [5.11, 5.11],
+      [5.12, 5.12],
+      [5.13, 5.13]
+    ]
+  end
+
+  def climb_type
+    [
+      ["Trad","Trad"],
+      ["Sport", "Sport"]
+    ]
+  end 
 end
