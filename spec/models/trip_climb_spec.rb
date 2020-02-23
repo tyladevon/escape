445294e20976ad8e05384df4b1ea24856fad8024
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 15:20:48 -0700

    Add attributes to trip climb and fix typos in validations

--- spec/models/trip_climb_spec.rb
+++ spec/models/trip_climb_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe TripClimb, type: :model do
   describe 'validations' do
-    it { validates_presence_of :trip_id}
-    it { validates_presence_of :climb_id}
+    it { should validate_presence_of :trip_id}
+    it { should validate_presence_of :climb_id}
   end
 
   describe 'relationships' do
-    it { belongs_to :trip}
-    it { belongs_to :climb}
+    it { should belong_to :trip}
+    it { should belong_to :climb}
+  end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      trip = create(:trip)
+      climb = create(:climb)
+      trip_climb = TripClimb.create!(trip: trip, climb: climb)
+
+      expect(trip_climb.trip_id).to eq(trip.id)
+      expect(trip_climb.climb_id).to eq(climb.id)
+    end
   end
 end
