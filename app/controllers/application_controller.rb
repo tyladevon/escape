282d67a9c5c74709fb496951f78bc26b3a2a6038
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 10:36:38 -0700

    Make helper method for available activities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  
+  helper_method :current_user, :available_activities
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def available_activities
+    @activities = ["climbing", "hiking"]
   end
 end
