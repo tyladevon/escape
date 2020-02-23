4a0dc426df0ba0f2a03107653c0f267593031386
Madelyn Romero <m.romero013@gmail.com>
2020-02-23 13:43:27 -0700

    Refactor test with stubbed call for destination and stubbed call for climbs

--- spec/features/search/user_can_fill_in_activity_preferences_spec.rb
+++ spec/features/search/user_can_fill_in_activity_preferences_spec.rb
@@ -3,13 +3,20 @@ require 'rails_helper'
 describe "As a logged-in User, after choosing destination and activity" do
   it "can select activity preferences (for climbing) and get back climbing routes" do
     user = create(:user)
-    
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    
-    # climb_results = File.read('/fixtures/climb_results.json')
-    
+
+    destination_fixture = File.read('spec/fixtures/los_angeles.json')
+
+    stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/destination/LosAngeles").
+    to_return(status: 200, body: destination_fixture)
+
+    climb_results = File.read('spec/fixtures/climb_results.json')
+
+    stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/climb_results?lat=34.0522342&lon=-118.2436849&max_dist=10&min_diff=5.7&max_diff=5.12").to_return(status: 200, body: climb_results)
+
     visit '/search'
-    
+
     fill_in "destination", with: "Los Angeles"
 
     within "#activity-climbing" do
@@ -18,21 +25,20 @@ describe "As a logged-in User, after choosing destination and activity" do
     end
 
     click_button "Continue"
-    
+
     expect(current_path).to eq('/search/climbs/new')
-    
+
     select "5.7", from: :min_diff
     select "5.12", from: :max_diff
     select "Sport", from: :type
-    fill_in :distance, with: "10" 
-    
-    # stub_request(:get, '/api/v1/suggestion').with(query: { user_info: user_info }).to_return(status: 200, body: json_response)
+    fill_in :distance, with: "10"
+
 
     click_on "Find Climbs!"
-    
+
     expect(current_path).to eq("/search/climbs")
-    
+
     expect()
     # expect(page).to have_content(climb_results["results"["success"]])
-  end  
+  end
 end
