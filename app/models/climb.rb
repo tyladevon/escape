0cca2148aa4388eb61ce53052a6cca96f243964c
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 15:57:54 -0700

    Add validations and relationships

--- app/models/climb.rb
+++ app/models/climb.rb
@@ -1,2 +1,19 @@
 class Climb < ApplicationRecord
+  validates_presence_of :route_id,
+                        :name,
+                        :climb_type,
+                        :rating,
+                        :stars,
+                        :pitches,
+                        :lat,
+                        :lng,
+                        :url
+
+  validates_numericality_of :stars,
+                            :pitches,
+                            :lat,
+                            :lng
+
+  has_many :trip_climbs
+  has_many :trips, through: :trip_climbs
 end
