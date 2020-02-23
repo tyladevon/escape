c8336728b877368ca5b7dc70edea714ef0a78462
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 15:03:44 -0700

    Add numericality and attribute asssertions

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -7,11 +7,31 @@ RSpec.describe Trip, type: :model do
     it { should validate_presence_of :destination_address}
     it { should validate_presence_of :lat}
     it { should validate_presence_of :lng}
+    it { should validate_numericality_of :lng}
+    it { should validate_numericality_of :lng}
   end
 
   describe 'relationships' do
-    it { belongs_to :user}
-    it { has_many :trip_climbs}
-    it { has_many(:climbs).through(:trip_climbs)}
+    it { should belong_to :user}
+    it { should have_many :trip_climbs}
+    it { should have_many(:climbs).through(:trip_climbs)}
+  end
+
+  describe 'attributes' do
+    it 'has attributes' do
+      user = create(:user)
+      trip = Trip.create!(name: "LA Trip",
+                          destination_name: "Los Angeles",
+                          destination_address: "Los Angeles, CA, USA",
+                          lat: 34.0522342,
+                          lng: -118.2436849,
+                          user: user)
+
+      expect(trip.name).to eq("LA Trip")
+      expect(trip.destination_name).to eq("Los Angeles")
+      expect(trip.destination_address).to eq("Los Angeles, CA, USA")
+      expect(trip.lat).to eq(34.0522342)
+      expect(trip.lng).to eq(-118.2436849)
+    end
   end
 end
