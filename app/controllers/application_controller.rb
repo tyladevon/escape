9ca54168497a5cb3787031b2f3f7f557ba709a74
Alison Vermeil <alison.minton@gmail.com>
2020-02-23 14:38:45 -0700

    Remove unnecessary available_activities method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :available_activities
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def available_activities
-    @activities = ["climbing", "hiking"]
   end
 end
