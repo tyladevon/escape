512fc28b4fa8e63038fb9f7379cf0473769e7b9e
Tyla Devon <tyladevon@gmail.com>
2020-02-24 08:24:14 -0700

    Refactor spec

--- spec/features/users/user_can_view_about_page_spec.rb
+++ spec/features/users/user_can_view_about_page_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe 'A User or Visitor' do
   it "can click on 'About' and learn about the application" do
-    # visit '/'
-    # click_on 'About'
     visit '/about'
     expect(current_path).to eq '/about'
     expect(".about").to_not be_empty
