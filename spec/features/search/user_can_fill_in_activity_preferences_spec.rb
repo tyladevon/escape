77eac12249a6ae29e2c21f9eea57c803f5b370d3
Madelyn Romero <m.romero013@gmail.com>
2020-02-24 16:02:10 -0700

    Delete unnecessary stubs and turn off VCR

--- spec/features/search/user_can_fill_in_activity_preferences_spec.rb
+++ spec/features/search/user_can_fill_in_activity_preferences_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 describe "As a logged-in User, after choosing destination and activity" do
+  VCR.turn_off! :ignore_cassettes => true
   it "can select activity preferences (for climbing) and get back climbing routes" do
+    WebMock.allow_net_connect!
+
     user = create(:user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    destination_fixture = File.read('spec/fixtures/los_angeles.json')
-
-    stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/destination/LosAngeles").
-    to_return(status: 200, body: destination_fixture)
-
-    climb_results = File.read('spec/fixtures/climb_results.json')
-
-    stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/climb_results").to_return(status: 200, body: climb_results)
+    # destination_fixture = File.read('spec/fixtures/boulder.json')
+    #
+    # stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/destination/Boulder").
+    #     to_return(status: 200, body: destination_fixture)
+    #
+    # climb_results = File.read('spec/fixtures/climb_results.json')
+    #
+    # stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/climb_results?lat=-105.25&lon=40.03&max_dist=10&min_diff=5.7&max_diff=5.12").to_return(status: 200, body: climb_results)
 
     visit '/search'
 
-    fill_in "destination", with: "Los Angeles"
+    fill_in "destination", with: "Boulder"
 
-    within "#activity-climbing" do
-      expect(page).to have_unchecked_field("selected_activity_")
-      check "selected_activity_"
-    end
+    choose "Climbing"
 
     click_button "Continue"
 
@@ -37,6 +37,6 @@ describe "As a logged-in User, after choosing destination and activity" do
     click_on "Find Climbs!"
 
     expect(current_path).to eq("/search/climbs")
-    expect(page).to have_css(".climb", count: 2)
+    expect(page).to have_css(".climb", count: 50)
   end
 end
