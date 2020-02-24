3f80bed9ac12b641cce2098bd5f1feb670b61eab
Madelyn Romero <m.romero013@gmail.com>
2020-02-24 16:11:14 -0700

    Delete commented out code and extra spaces

--- app/controllers/search/climbs_controller.rb
+++ app/controllers/search/climbs_controller.rb
@@ -4,13 +4,6 @@ class Search::ClimbsController < ApplicationController
     @destination = Destination.new(session[:destination])
   end
 
-  # def create
-  #   location = session[:destination]
-  #
-  #   EscapeService.new.get_climb_results(params, location)
-  #   redirect_to "/search/climbs"
-  # end
-
   def index
     location = session[:destination]
     render locals: {
--- spec/features/search/user_can_fill_in_activity_preferences_spec.rb
+++ spec/features/search/user_can_fill_in_activity_preferences_spec.rb
@@ -9,15 +9,6 @@ describe "As a logged-in User, after choosing destination and activity" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    # destination_fixture = File.read('spec/fixtures/boulder.json')
-    #
-    # stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/destination/Boulder").
-    #     to_return(status: 200, body: destination_fixture)
-    #
-    # climb_results = File.read('spec/fixtures/climb_results.json')
-    #
-    # stub_request(:get, "https://escape-app-api.herokuapp.com/api/v1/climb_results?lat=-105.25&lon=40.03&max_dist=10&min_diff=5.7&max_diff=5.12").to_return(status: 200, body: climb_results)
-
     visit '/search'
 
     fill_in "destination", with: "Boulder"
@@ -32,7 +23,6 @@ describe "As a logged-in User, after choosing destination and activity" do
     select "5.12", from: :max_diff
     select "Sport", from: :type
     fill_in :distance, with: "10"
-
 
     click_on "Find Climbs!"
 
