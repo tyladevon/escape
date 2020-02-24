0750d42805c4f563bb436d12167ffea3c0d5f262
Tyla Devon <tyladevon@gmail.com>
2020-02-24 08:25:23 -0700

    Add conditional to User's Controller for show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-
   def show
+    if current_user.nil? 
+      flash[:error] = "Please log in to continue"
+      redirect_to '/'
+    end
   end
-
 end
