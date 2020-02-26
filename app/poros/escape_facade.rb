1c09434daf14dbddee038645101196ef08910582
Tyla Devon <tyladevon@gmail.com>
2020-02-26 16:22:33 -0700

    Co-authored-by: rallenwasd@gmail.com
    Refactor for User can choose two types of climbs

--- app/poros/escape_facade.rb
+++ app/poros/escape_facade.rb
@@ -5,11 +5,17 @@ class EscapeFacade
   end 
 
   def climb_options
-      results = EscapeService.new.get_climb_results(@params, @location)
-      results.map do |result|
-        if result[:type] == @params[:type] || result[:type] == "Sport, TR"
-          ClimbOptions.new(result)
-        end 
-      end.compact
-    end
+    results = EscapeService.new.get_climb_results(@params, @location)
+    results.map do |result|
+      if @params.key?(:trad) && @params.key?(:sport) && (result[:type] == "Trad" || result[:type] == "Sport" || result[:type] == "Sport, TR")
+        ClimbOptions.new(result)
+      elsif !@params.key?(:trad) && !@params.key?(:sport) && (result[:type] == "Trad" || result[:type] == "Sport" || result[:type] == "Sport, TR")
+        ClimbOptions.new(result)
+      elsif @params.key?(:trad) && result[:type] == "Trad"
+        ClimbOptions.new(result)
+      elsif @params.key?(:sport) && (result[:type] == "Sport" || result[:type] == "Sport, TR")
+        ClimbOptions.new(result)
+      end
+    end.compact
   end
+end
