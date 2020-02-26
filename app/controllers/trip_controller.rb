5dc6848024169310697f24829dd25a8a03bd4ec4
Tyla Devon <tyladevon@gmail.com>
2020-02-26 11:04:21 -0700

    Refactor new method to include error handling for search

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -2,19 +2,27 @@ class TripController < ApplicationController
 
   def new
     @location = session[:destination]
-    if params["min_diff"] < params["max_diff"]
-      flash[:error] = "Please ensure your minimium difficulty is less than your maximum difficulty in your selection preferences"
+
+    diff_list = ["5.6", "5.7","5.8", "5.9", "5.10", "5.11", "5.12", "5.13"]
+    
+    min_diff = diff_list.find_index(params["min_diff"])
+    max_diff = diff_list.find_index(params["max_diff"])
+
+    if min_diff > max_diff && params["distance"].to_i <= 0
+      flash[:error] = "Please ensure your minimium difficulty is less than your maximum difficulty in your selection preferences and you have a distance of at least 1 mile"
       redirect_to '/search/climbs/new'
     elsif params["distance"].to_i <= 0
       flash[:error] = "Please fill in a distance of atleast 1 and try your search again!"
       redirect_to '/search/climbs/new'
+    elsif min_diff > max_diff && params["distance"].to_i <= 0
+      flash[:error] = "Please ensure your minimium difficulty is less than your maximum difficulty in your selection preferences"
+      redirect_to '/search/climbs/new'
     else
-    render locals: {
-      climb_results: EscapeFacade.new(params, @location).climb_options
-    }
+      escape_facade = EscapeFacade.new(params, @location).climb_options
+      validate_climb_results(escape_facade)
     end
-  end 
-  
+  end
+
   def create
     destination = session["destination"]
     new_trip = current_user.trips.create(
@@ -39,5 +47,18 @@ class TripController < ApplicationController
       )
     end
     redirect_to trip_path(new_trip)
+  end
+
+  private 
+
+  def validate_climb_results(escape_facade)
+    if escape_facade.length == 0 
+        flash[:error] = "No results returned, please adjust your search and try again"
+        redirect_to '/search/climbs/new'
+    else
+      render locals: {
+        climb_results: escape_facade
+      }
+    end
   end
 end
