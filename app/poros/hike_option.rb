d4ab8fcc7e7f13efaa2687f8098ffbe04603fe26
Madelyn Romero <m.romero013@gmail.com>
2020-02-26 16:27:38 -0700

    Add hike attributes
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- app/poros/hike_option.rb
+++ app/poros/hike_option.rb
@@ -1,12 +1,19 @@
 class HikeOption
 
-  attr_reader :name, :url, :lat, :lng
+  attr_reader :name, :url, :lat, :lng, :summary, :difficulty, :stars, :location, :length, :ascent, :max_elevation
 
   def initialize(attributes)
     @name = attributes[:name]
     @url = attributes[:url]
     @lat = attributes[:latitude]
     @lng = attributes[:longitude]
+    @summary = attributes[:summary]
+    @difficulty = attributes[:difficulty]
+    @stars = attributes[:stars]
+    @location = attributes[:location]
+    @length = attributes[:length]
+    @ascent = attributes[:ascent]
+    @max_elevation = attributes[:high]
   end
 
 end
