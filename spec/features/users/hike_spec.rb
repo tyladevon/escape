b395b5d153a51c31504f0b5ab7c59f4efe18666f
Madelyn Romero <m.romero013@gmail.com>
2020-02-26 16:25:27 -0700

    Add within blocks for more robust testing
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- spec/features/users/hike_spec.rb
+++ spec/features/users/hike_spec.rb
@@ -19,16 +19,35 @@ RSpec.describe "Hikes" do
 
     fill_in "distance", with: 1
     fill_in "max_results", with: 3
+    fill_in 'min_stars', with: 0
 
 
     click_button "Find Hikes"
 
     expect(current_path).to eq("/hikes/trips/new")
 
-    expect(page).to have_content("Dakota Ridge to Sanitas Valley Loop")
-    expect(page).to have_content("Mount Sanitas Loop")
-    expect(page).to have_content("Sunshine Lion's Lair Loop")
     expect(page).to have_css(".hike", count: 3)
+    expect(page).to have_content("3 results")
+
+    within "#hike-result-0" do
+      expect(page).to have_link("Dakota Ridge to Sanitas Valley Loop", href: "https://www.hikingproject.com/trail/7006781/dakota-ridge-to-sanitas-valley-loop")
+      expect(page).to have_content("A technical ascent with a great view of Boulder, followed by a nice, open downhill pitch.")
+      expect(page).to have_content("greenBlue")
+      expect(page).to have_content(4)
+      expect(page).to have_content("Boulder, Colorado")
+      expect(page).to have_content(2.3)
+      expect(page).to have_content(458)
+      expect(page).to have_content(5961)
+    end
+
+    within "#hike-result-1" do
+      expect(page).to have_content("Mount Sanitas Loop")
+    end
+
+    within "#hike-result-2" do
+      expect(page).to have_content("Sunshine Lion's Lair Loop")
+    end
+
     expect(page).to have_button("Save Selected Hikes")
 
     check "check-box-0"
@@ -47,4 +66,7 @@ RSpec.describe "Hikes" do
       expect(page).to have_css(".hike", count: 3)
     end
   end
+  #need to do a test where you enter stars
+  #test if you don't enter stars
+
 end
