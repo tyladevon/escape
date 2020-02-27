455a67c7da949f54f594afd4b2c1967303118dd3
Madelyn Romero <m.romero013@gmail.com>
2020-02-26 18:02:09 -0700

    Add validations and relationship with trips
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- spec/models/hike_spec.rb
+++ spec/models/hike_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Hike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :url}
+    it {should validate_presence_of :lat}
+    it {should validate_presence_of :lng}
+  end
+
+  describe "Relationships" do
+    it {should belong_to :trip}
+  end
 end
