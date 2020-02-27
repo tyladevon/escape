1a00ca9d0484d44409c8e11e62e3ac69764e73bb
Madelyn Romero <m.romero013@gmail.com>
2020-02-26 17:24:30 -0700

    Add checks for attributes for hikes and map on trip show page
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- spec/features/users/hike_spec.rb
+++ spec/features/users/hike_spec.rb
@@ -67,7 +67,27 @@ RSpec.describe "Hikes" do
 
     within(".hikes") do
       expect(page).to have_css(".hike", count: 3)
+      within "#hike-#{Hike.first.id}" do
+        expect(page).to have_link("Dakota Ridge to Sanitas Valley Loop", href: "https://www.hikingproject.com/trail/7006781/dakota-ridge-to-sanitas-valley-loop")
+        expect(page).to have_content("A technical ascent with a great view of Boulder, followed by a nice, open downhill pitch.")
+        expect(page).to have_content("greenBlue")
+        expect(page).to have_content(4)
+        expect(page).to have_content("Boulder, Colorado")
+        expect(page).to have_content(2.3)
+        expect(page).to have_content(458)
+        expect(page).to have_content(5961)
+      end
+
+      within "#hike-#{Hike.second.id}" do
+        expect(page).to have_content("Mount Sanitas Loop")
+      end
+
+      within "#hike-#{Hike.last.id}" do
+        expect(page).to have_content("Sunshine Lion's Lair Loop")
+      end
     end
+
+    expect(page).to have_css("#map")
   end
   #need to do a test where you enter stars
   #test if you don't enter stars
