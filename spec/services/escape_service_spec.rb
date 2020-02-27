51c8d4672b82271cf1042710d01b0c7c6a7b2305
Madelyn Romero <m.romero013@gmail.com>
2020-02-26 17:43:13 -0700

    Add test for get hike results API call
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- spec/services/escape_service_spec.rb
+++ spec/services/escape_service_spec.rb
@@ -18,5 +18,35 @@ describe EscapeService do
         expect(location_info[:data][:attributes]).to have_key :longitude
       end
     end
+
+    describe "#get_hike_results" do
+      it "returns the hikes with given parameters", :vcr do
+        service = EscapeService.new
+        params = {"max_dist" => 1,
+                  "max_results" => 3,
+                  "min_stars" => 0}
+        lat = 40.03
+        lng = -105.25
+
+        hikes_info = service.get_hike_results(params, lat, lng)
+
+        expect(hikes_info.length).to eq(3)
+        expect(hikes_info.first).to have_key :name
+        expect(hikes_info.first).to have_key :rid
+        expect(hikes_info.first).to have_key :type
+        expect(hikes_info.first).to have_key :summary
+        expect(hikes_info.first).to have_key :difficulty
+        expect(hikes_info.first).to have_key :stars
+        expect(hikes_info.first).to have_key :location
+        expect(hikes_info.first).to have_key :length
+        expect(hikes_info.first).to have_key :ascent
+        expect(hikes_info.first).to have_key :descent
+        expect(hikes_info.first).to have_key :longitude
+        expect(hikes_info.first).to have_key :latitude
+        expect(hikes_info.first).to have_key :url
+        expect(hikes_info.first).to have_key :high
+        expect(hikes_info.first).to have_key :low
+      end
+    end
   end
 end
