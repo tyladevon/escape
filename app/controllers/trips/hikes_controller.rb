dd7e2a068e3dd08940c62b943c053f7e7259bac1
Alison Vermeil <alison.minton@gmail.com>
2020-02-26 22:34:43 -0700

    Refactor controller

--- app/controllers/trips/hikes_controller.rb
+++ app/controllers/trips/hikes_controller.rb
@@ -6,9 +6,7 @@ class Trips::HikesController < ApplicationController
   def index
     @trip = Trip.find(params[:trip_id])
 
-    lat = @trip.lat
-    lng = @trip.lng
-    all_hikes = EscapeService.new.get_hike_results(params, lat, lng)
+    all_hikes = EscapeService.new.get_hike_results(params, @trip.lat, @trip.lng)
     @hike_options = all_hikes.map do |hike_response|
       HikeOption.new(hike_response)
     end
@@ -16,26 +14,38 @@ class Trips::HikesController < ApplicationController
 
   def create
     trip = Trip.find(params[:trip_id])
-
+    trip_rids = trip.hikes.pluck(:rid)
     hikes = params["hike_objects"]
-
+    count = 0
     hikes.each do |hike|
-      parsed_hike = JSON.parse(hike)
-      trip.hikes.create!(
-        name: parsed_hike["name"],
-        url: parsed_hike["url"],
-        lat: parsed_hike["lat"],
-        lng: parsed_hike["lng"],
-        summary: parsed_hike["summary"],
-        difficulty: parsed_hike["difficulty"],
-        stars: parsed_hike["stars"],
-        location: parsed_hike["location"],
-        length: parsed_hike["length"],
-        ascent: parsed_hike["ascent"],
-        high: parsed_hike["max_elevation"]
-      )
-
+      hike_params = parsed_hike(hike)
+      if trip_rids.include?(hike_params[:rid])
+        count +=1
+      else
+        trip.hikes.create!(hike_params)
+      end
     end
+    flash[:notice] = 'One or more of the hikes you selected are already saved to this trip. Only new hikes were added.' if count >= 1
     redirect_to trip_path(trip)
+  end
+
+  private
+
+  def parsed_hike(hike_data)
+    parsed_data = JSON.parse(hike_data)
+    {
+     name: parsed_data["name"],
+     rid: parsed_data["rid"],
+     url: parsed_data["url"],
+     lat: parsed_data["lat"],
+     lng: parsed_data["lng"],
+     summary: parsed_data["summary"],
+     difficulty: parsed_data["difficulty"],
+     stars: parsed_data["stars"],
+     location: parsed_data["location"],
+     length: parsed_data["length"],
+     ascent: parsed_data["ascent"],
+     high: parsed_data["max_elevation"]
+   }
   end
 end
